5b65545d6ef58c438a034cf3cff02c44a3975240
ligrev <1475200+ligrevx@users.noreply.github.com>
2020-08-03 16:23:54 +0800

    Add files via upload

---
+++ string2hex.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+int main(int argc, char **argv) {
+
+  if( argc != 2) {
+    printf("usage: %s \"convert this string to hex\"", argv[0] );
+    exit(0);
+  }
+
+  for (int i=0; i < strlen(argv[1]); i++)
+    printf("%2x", argv[1][i]);
+
+  return 0;
+}
